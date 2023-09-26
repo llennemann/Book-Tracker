a794d6d03c558e308f4208edc52374207e4ef1d5
Lucy Lennemann <llennema@ucsd.edu>
2023-09-26 08:47:49 -0700

    Fixed mapping function for displaying books, exclude tester books

--- my-app/src/components/BookList.js
+++ my-app/src/components/BookList.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import image from "./mytest.jpeg";
 
 const BookList = () => {
-    const [bookList, setBookList] = useState('');
+    const [bookList, setBookList] = useState([]);
 
     useEffect(() => {
         const getBooks = () => {
@@ -12,14 +12,24 @@ const BookList = () => {
                 setBookList(res.data);
             });
         }
-
+        
         getBooks();
     }, []);
 
     return (
         <>
         <div className="book-list">
-            
+            {bookList.map(book => {
+                if (book.title === null || book.title.includes('Test')) {
+                    return null;
+                }
+                else {
+                    return <div key={book._id} className="book">
+                                <h2>{book.title}</h2>
+                                <img src={book.image}></img>
+                            </div>
+                }
+            })}
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
