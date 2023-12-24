a75e093dc730d1e20b1e20e5a532750497217162
Lucy Lennemann <llennema@ucsd.edu>
2023-12-23 22:50:46 -0800

    Delete bk route, prevent duplicates from being added

--- backend/src/routes/book.js
+++ backend/src/routes/book.js
@@ -12,19 +12,40 @@ router.get('/getlist', async (req, res) => {
 })
 
 // add a book into the database
-// todo - beware of duplicates????
 router.post('/addbook', async (req, res) => {
     console.log(req.body);
     const { title, author, image } = req.body;
     
-    const result = await db.bookdb.collection('books').insertOne({
-        title: title,
-        author: author,
-        image: image
-    });
-    res.sendStatus(200);
+    const foundBk = await db.bookdb.collection('books').findOne( {title: title, author: author} );
+
+    console.log(foundBk);
+
+    if (foundBk == null) {
+        const result = await db.bookdb.collection('books').insertOne({
+            title: title,
+            author: author,
+            image: image
+        });
+        res.sendStatus(200);
+    }
+    else {
+        res.sendStatus(404); // means book is already in database
+    }
 })
 
 // remove a book from the database
+// input: title and author should be the same
+// delete first document that matches the condition
+router.post('/deletebook', async (req, res) => {
+    const { title, author } = req.body;
+
+    const result = await db.bookdb.collection('books').deleteOne({ title: title, author: author });
+    if (result['deletedCount'] > 0) {
+        res.sendStatus(200);
+    }
+    else {
+        res.sendStatus(404); // error in deleting
+    }
+})
 
 module.exports = router;
